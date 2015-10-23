55e7830415f858b19deeb10eaa916f79f60f2fa7
Brian Jacobel <bjacobel@gmail.com>
2015-10-23 11:50:38 -0400

    Switch to using real-time messaging API

--- slact.go
+++ slact.go
@@ -1,50 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"net/http"
 	"os"
 
-	"github.com/bjacobel/slact/Godeps/_workspace/src/github.com/bjacobel/fresh/runner/runnerutils"
 	"github.com/bjacobel/slact/Godeps/_workspace/src/github.com/davecgh/go-spew/spew"
-	"github.com/bjacobel/slact/Godeps/_workspace/src/github.com/go-martini/martini"
 	"github.com/bjacobel/slact/Godeps/_workspace/src/github.com/joho/godotenv"
-	"github.com/bjacobel/slact/Godeps/_workspace/src/github.com/martini-contrib/binding"
-	"github.com/bjacobel/slact/Godeps/_workspace/src/github.com/martini-contrib/gorelic"
-	"github.com/bjacobel/slact/Godeps/_workspace/src/github.com/martini-contrib/render"
-	"github.com/bjacobel/slact/models"
+	"github.com/bjacobel/slact/Godeps/_workspace/src/github.com/nlopes/slack"
 )
 
 var spw = spew.NewDefaultConfig()
-
-func runnerMiddleware(w http.ResponseWriter, r *http.Request) {
-	if runnerutils.HasErrors() {
-		runnerutils.RenderError(w)
-	}
-}
 
 func main() {
 	// Add some configuration to our JSON logger
 	spw = &spew.ConfigState{Indent: "\t", MaxDepth: 5}
 
-	app := martini.Classic()
-	app.Use(render.Renderer())
-
 	err := godotenv.Load()
 	if err != nil {
-		// If it's the dev environment, use Fresh as a reloader
-		app.Use(runnerMiddleware)
-	} else {
-		gorelic.InitNewrelicAgent(os.Getenv("NRKEY"), "slact", false)
-		app.Use(gorelic.Handler)
+		log.Fatal(err)
 	}
 
-	app.Group("/v1", func(r martini.Router) {
-		r.Post("/messages", binding.Form(models.WebhookMsg{}), func(msg models.WebhookMsg, r render.Render) {
-			log.Println(msg.Text)
-			r.JSON(200, nil)
-		})
-	})
+	api := slack.New(os.Getenv("SLACK"))
 
-	app.Run()
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+	for {
+		select {
+		case msg := <-rtm.IncomingEvents:
+			switch msg.Data.(type) {
+
+			case *slack.ReactionAddedEvent:
+				go handleReaction(msg.Data.(*slack.ReactionAddedEvent), rtm)
+			default:
+				continue
+			}
+		}
+	}
+}
+
+func handleReaction(reaction *slack.ReactionAddedEvent, rtm *slack.RTM) {
+	rtm.SendMessage(
+		rtm.NewOutgoingMessage(
+			fmt.Sprintf("New reaction added :%s:", reaction.Reaction),
+			reaction.Item.Item.Channel,
+		),
+	)
 }
